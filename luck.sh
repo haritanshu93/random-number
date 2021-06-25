e54ffc04c891d2b8f3935002434417bd73aa3b94
who1243 <77314088+who1243@users.noreply.github.com>
2021-06-25 10:39:05 +0530

    Add files via upload

---
+++ luck.sh
@@ -0,0 +1,23 @@
+random=$((1 + $RANDOM % 10))
+
+echo 'enter a random number'
+read r
+if [[ ( $r -eq $random ) ]]
+then
+        echo 'great' $1
+else
+        echo 'nope' $1 'the number was' $random
+fi
+
+echo 'want to play again(1 for play again 2 for dont play again)'
+read m
+if [[ ( $m -eq 1 ) ]]
+then
+        bash luck.sh
+elif [[ ( $m -eq 2 ) ]]
+then
+        echo 'end of game'
+else
+echo 'not an input' $1
+fi
+
